76bb42d4f6c723b80384ce654de689286410b144
Raymond Cohen <rcohen@squareup.com>
2024-11-25 14:53:45 -0500

    add test coverage demonstrating CSS not ordered alphabetically by addon name

--- tests/scenarios/compat-addon-styles-test.ts
+++ tests/scenarios/compat-addon-styles-test.ts
@@ -1,6 +1,6 @@
 import type { ExpectFile } from '@embroider/test-support/file-assertions/qunit';
 import { expectFilesAt, expectRewrittenFilesAt } from '@embroider/test-support/file-assertions/qunit';
-import type { PreparedApp } from 'scenario-tester';
+import type { PreparedApp, Project } from 'scenario-tester';
 import { throwOnWarnings } from '@embroider/core';
 import { appScenarios, baseAddon } from './scenarios';
 import QUnit from 'qunit';
@@ -129,6 +129,61 @@ appScenarios
         expectRewrittenFile('./node_modules/my-addon3/my-addon3.css').matches(`from-addon`);
         expectRewrittenFile('./node_modules/my-addon3/outer.css').matches(`from-outer`);
         expectRewrittenFile('./node_modules/my-addon3/nested/inner.css').matches(`from-inner`);
+      });
+    });
+  });
+
+function setAddonNameAndCSS(addon: Project, letter: string) {
+  addon.pkg.name = `addon-${letter}`;
+  merge(addon.files, {
+    addon: {
+      styles: {
+        'addon.css': `.addon-${letter} {}`,
+      },
+    },
+  });
+}
+
+appScenarios
+  .map('compat-addon-styles', project => {
+    const addonA: Project = baseAddon();
+    setAddonNameAndCSS(addonA, 'a');
+
+    const addonB = baseAddon();
+    setAddonNameAndCSS(addonB, 'b');
+
+    const addonC = baseAddon();
+    setAddonNameAndCSS(addonC, 'c');
+
+    const addonD = baseAddon();
+    setAddonNameAndCSS(addonD, 'd');
+
+    project.addDependency(addonA);
+    project.addDependency(addonB);
+    addonA.addDependency(addonC);
+    project.addDependency(addonD);
+  })
+  .forEachScenario(scenario => {
+    Qmodule(scenario.name, function (hooks) {
+      throwOnWarnings(hooks);
+
+      let app: PreparedApp;
+
+      let expectFile: ExpectFile;
+
+      hooks.before(async assert => {
+        app = await scenario.prepare();
+        let result = await app.execute('ember build', { env: { STAGE2_ONLY: 'true' } });
+        assert.equal(result.exitCode, 0, result.output);
+      });
+
+      hooks.beforeEach(assert => {
+        expectFile = expectFilesAt(app.dir, { qunit: assert });
+      });
+
+      test('addon styles are in order', function () {
+        const vendorCssPath = './node_modules/.embroider/rewritten-app/assets/vendor.css';
+        expectFile(vendorCssPath).matches(/addon-a.*addon-b.*addon-c.*addon-d/);
       });
     });
   });
